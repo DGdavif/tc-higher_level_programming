1697b12d614520a31d831d892cf9f5805ff96754
DGdavif <davimenezes1506@gmail.com>
2025-09-09 22:44:34 +0000

    Adiciona função islower (verifica se caractere é minúsculo)

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-islower = _import_('7-islower').islower
+def islower(c):
+    """Retorna True se c for letra minúscula, caso contrário False."""
+    if not isinstance(c, str) or len(c) != 1:
+        return False
+    return ord('a') <= ord(c) <= ord('z')
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
