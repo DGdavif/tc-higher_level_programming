388ac593b75db583352af47c33c9a46951f956bf
DGdavif <davimenezes1506@gmail.com>
2025-09-11 22:32:55 +0000

    Add function pow for task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    """Retorna True se c for letra minúscula, caso contrário False."""
-    if not isinstance(c, str) or len(c) != 1:
-        return False
     return ord('a') <= ord(c) <= ord('z')
-
