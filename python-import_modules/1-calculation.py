d18435b092d6da09827a220b6876fc7a42306a56
DGdavif <davimenezes1506@gmail.com>
2025-09-18 22:51:50 +0000

    Add 1-calculation.py with basic math operations

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+def add(a, b):
+    return (a + b)
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
+def sub(a, b):
+    return (a - b)
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return int(a / b)
