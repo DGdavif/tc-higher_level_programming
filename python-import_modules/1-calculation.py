6ecd8eacaa3cbb59ec3fb1fe43aa16548658e97d
DGdavif <davimenezes1506@gmail.com>
2025-09-19 18:13:24 +0000

    Add calculation program using calculator_1 functions

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+def add(a, b):
+    """My addition function"""
+    return (a + b)
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+def sub(a, b):
+    """My subtraction function"""
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function"""
+    return (a * b)
+
+
+def div(a, b):
+    """My division function"""
+    return int(a / b)
